cd4dabe99f39838c1b2341fbffb44c63f3eb9d2a
liyiming <liyiming0215@sina.com>
2018-02-09 11:45:37 +0800

    springboot 学习

---
+++ src/main/java/test/springboot/hello/Application.java
@@ -0,0 +1,23 @@
+/**
+ * @author:liyiming
+ * @date:2018年2月8日
+ * Description:
+ **/
+package test.springboot.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Title: Application Description: Company:pusense
+ * 
+ * @author ：lyiming
+ * @date ：2018年2月8日
+ **/
+@SpringBootApplication
+public class Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/test/springboot/hello/GreetingController.java
@@ -0,0 +1,26 @@
+/**
+ * @author:liyiming
+ * @date:2018年2月8日
+ * Description:
+ **/
+package test.springboot.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Title: GreetingController Description: Company:pusense
+ * 
+ * @author ：lyiming
+ * @date ：2018年2月8日
+ **/
+@Controller
+public class GreetingController{
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name",required=false,defaultValue="world") String name,Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
