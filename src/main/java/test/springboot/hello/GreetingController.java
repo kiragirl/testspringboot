28772cb62e5d514efc82e12dad42a77e15f7bd0f
liyiming <liyiming0215@sina.com>
2018-02-26 16:46:45 +0800

    阅读进度https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-caching

--- src/main/java/test/springboot/hello/GreetingController.java
+++ src/main/java/test/springboot/hello/GreetingController.java
@@ -5,17 +5,23 @@
  **/
 package test.springboot.hello;
 
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import test.springboot.hello.bean.User;
 import test.springboot.hello.bean.UserBean;
 import test.springboot.hello.config.FooProperties;
+import test.springboot.hello.repository.UserRepository;
 
 /**
  * Title: GreetingController Description: Company:pusense
@@ -31,7 +37,13 @@ public class GreetingController{
 	private UserBean userBean;
 	@Autowired
 	private FooProperties properties;
-
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "world") String name,
 			Model model) {
@@ -42,14 +54,23 @@ public class GreetingController{
 	@RequestMapping("/greeting2")
 	public String greeting2(Model model) {
 		// String name = userBean.getName();
-		String name = userBean.getExternalConfigRandomValue();
+		String name = "";
+		List<Map<String, Object>> list = jdbcTemplate.queryForList("select username from test_user where test_user.id=?",
+				"141111153857816nrtjl");
+		for (Map<String, Object> map : list) {
+			logger.debug("the user's username is {}", map.get("username"));
+			name = (String) map.get("username");
+		}
+		//name = userBean.getExternalConfigRandomValue();
 		model.addAttribute("name", name);
 		return "greeting";
 	}
 
 	@RequestMapping("/greeting3")
 	public String greeting3(Model model) {
-		System.out.println("Hello \u001b[1;31mred\u001b[0m world!"); 
+		User user = userRepository.findUserById("123");
+		logger.debug(" userRepository.findUserById:{}",user.getUsername());
+		System.out.println("Hello \u001b[1;31mred\u001b[0m world!");
 		logger.debug("this is a test debug log");
 		logger.info("this is a test info log");
 		logger.warn("this is a test warn log");
